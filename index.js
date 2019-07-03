0a49fc5f55b88d079655f31535fcb168be21adad
SnowSurfer <drinkaziu@gmail.com>
2019-07-03 12:22:39 -0400

    set up mongo and mongoose

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
@@ -7,5 +11,7 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+
