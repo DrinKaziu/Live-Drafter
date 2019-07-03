1524929446c7f52869b0d79efb81ea7dd6722532
SnowSurfer <drinkaziu@gmail.com>
2019-07-03 12:40:30 -0400

    create User schema

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,25 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('profile:', profile)
+    User.findOne({ googleId: profile.id }).then((existingUser) => {
+      if(existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user))
+      }
+    }) 
   })
 );
