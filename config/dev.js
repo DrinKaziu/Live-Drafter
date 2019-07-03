3568a66dcea2be28cdd2f9405aec44ab81ffff65
SnowSurfer <drinkaziu@gmail.com>
2019-07-03 14:07:18 -0400

    update .gitignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '326846018176-0hiaajhipbum759f91j9dpup8b91tu77.apps.googleusercontent.com',
-  googleClientSecret: 'nf-jl6raJg7z_KE4APWp2or9',
-  mongoURI: 'mongodb+srv://admin:gjLyNZ8qHvmImY0b@live-drafter-dev-mlfzp.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'nachotheadorabledog'
-};
